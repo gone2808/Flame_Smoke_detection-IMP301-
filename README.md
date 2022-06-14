3835585ef6f73adbd53a3bbfaaf8fa9e1a27d892
gone2808 <nguyenmoe2k1@gmail.com>
2022-06-14 10:31:49 +0700

    git init

--- README.md
+++ README.md
@@ -1,1 +1,28 @@
-# Flame_Smoke_detection-IMP301-
+# Flame_Smoke_Detection
+
+- This is the project for IMP301(assignment 1).
+- The implementation is simplely based on the details paper on reference.
+
+## Docs
+
+- [Summary detail](docs/summary.pdf)
+
+## Demo
+
+<div align="center">
+ <img height="200em" src="gif_and_image/barbeq_result.gif">
+ <img height="200em" src="gif_and_image/controlled1_result.gif"/>
+</div>
+<div align="center">
+ <img height="200em" src="gif_and_image/fire1_result.gif">
+ <img height="200em" src="gif_and_image/controlled2_result.gif"/>
+</div>
+<div align="center">
+ <img height="200em" src="gif_and_image/forest1_result.gif">
+</div>
+
+## References
+
+- [A real-time video fire flame and smoke detection algorithm - Chunyu Yu*, Zhibin Mei, Xi Zhang](https://www.sciencedirect.com/science/article/pii/S1877705813013222)
+- [Real Time Video Based-Early Fire Smoke Detection for Outdoor Environment - Pham N. Giao 1 , Vuong P. Thanh 2 , Ngo T. Son 3 , Nguyen H. Phong 4 , Pham N. Khoa 5 , and Ki-Ryong
+Kwon 6](https://www.fpt-software.com/wp-content/uploads/sites/2/2018/03/Real-Time-Video-Based-Early-Fire-Smoke-Detection-for-Outdoor-Environment.pdf)
---
+++ demo.py
@@ -0,0 +1,43 @@
+import numpy as np
+import cv2 as cv
+import pybgs as bgs
+import math
+from scipy.signal import convolve
+from FSdetection import flame_smoke_detection
+
+def save_to_video(frames,save_name):
+    fourcc = cv.VideoWriter_fourcc('M','J','P','G')
+    out = cv.VideoWriter(save_name, fourcc, 30, frames[0].shape[:2][::-1])
+    for i in range(len(frames)):
+        out.write(frames[i])
+    out.release()
+
+if __name__ == '__main__':
+    video_path = 'data/controlled2.avi'
+    save_name = video_path.split('/')[-1].split('.')[0] + '_result.avi'
+    cap = cv.VideoCapture(video_path)
+    vid_fps = cap.get(cv.CAP_PROP_FPS)
+    vid_height = int(cap.get(cv.CAP_PROP_FRAME_HEIGHT))
+    vid_width = int(cap.get(cv.CAP_PROP_FRAME_WIDTH))
+
+    fs_detect = flame_smoke_detection()
+    fs_detect.video_info_initialize(vid_height, vid_width, vid_fps)
+
+    frames = []
+    while(cap.isOpened()):
+        ret, frame = cap.read()
+        if( ret == False ):
+            break
+        
+        # cv.imshow('frame', frame)
+        frames.append(fs_detect.apply(frame))
+            
+        k = cv.waitKey(50) & 0xff
+        if k == 27:
+            cv.destroyAllWindows()
+            break
+
+
+    cap.release()
+    cv.destroyAllWindows()
+    save_to_video(frames, 'result/' + save_name)
